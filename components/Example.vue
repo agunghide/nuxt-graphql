4192c4a2a7fb3c7b56851d4a797b6bdfbde370d9
Agung Hidayat <agunghidayat10@gmail.com>
2023-12-01 13:55:08 +0700

    feat: add example query

---
+++ components/Example.vue
@@ -0,0 +1,35 @@
+<!-- Please remove this file from your project -->
+<template>
+  <code>
+    {{
+      JSON.stringify(response)
+    }}
+  </code>
+</template>
+
+<script>
+import capsulesQuery from '@/query/capsules'
+
+export default {
+  name: 'NuxtExample',
+  data() {
+    return {
+      response: null
+    }
+  },
+  mounted() {
+    this.getData()
+  },
+  methods: {
+    async getData() {
+      try {
+        this.response = await this.$apollo.query({
+          query: capsulesQuery
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+</script>
